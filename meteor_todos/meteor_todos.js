7d15b7060704809384337bd03cc3e877133b926e
cxhandley <handley.craig@gmail.com>
2015-07-04 15:52:12 +1000

    Add Session - Hide Completed Tasks and Task Count

--- meteor_todos/meteor_todos.js
+++ meteor_todos/meteor_todos.js
@@ -3,9 +3,23 @@ if (Meteor.isClient) {
   Django = DDP.connect('http://'+window.location.hostname+':8000/');
   Tasks = new Mongo.Collection("django_todos.task", {"connection": Django});
   Django.subscribe('Tasks');
+
+
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {created_at: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {created_at: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {created_at: -1}});
+      }
+    },
+    hideCompleted: function() {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function() {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -16,14 +30,15 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text
-      }, function(error, _id) {
-        console.log('_id: ' + _id);
-      });
+      }); // created_at set on the server
 
       event.target.text.value = "";
 
       // Prevent default form submit
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
@@ -31,7 +46,6 @@ if (Meteor.isClient) {
     "click .toggle-checked": function() {
       // set the checked property to the opposite of its current value
       Tasks.update(this._id, {$set: {checked: ! this.checked}});
-      console.log('updated');
     },
     "click .delete": function() {
       Tasks.remove(this._id);
