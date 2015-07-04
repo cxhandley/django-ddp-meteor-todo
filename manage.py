86eb8a4c65e0b9b587a685ad56e8d602797dfffd
cxhandley <handley.craig@gmail.com>
2015-07-04 15:16:23 +1000

    Updated folder name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoddpmeteor.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-ddp-meteor-todo.settings")
 
     from dddp import greenify
     greenify()
